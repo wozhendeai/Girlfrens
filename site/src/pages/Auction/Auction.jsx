2320d8e881f7bfc302c2efe5c543f6a79ab1f089
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-02-03 08:47:03 -0500

    add comments to auction page

--- site/src/pages/Auction/Auction.jsx
+++ site/src/pages/Auction/Auction.jsx
@@ -13,36 +13,45 @@ function Auction() {
   return (
     <Container className="auction-container">
       <Row>
+        {/* NFT image */}
         <Col lg={6} className="mb-4 image-col">
           <Card className="border-0">
             <Card.Img src={ImageThree} alt="Auction Item" className="img-fluid" />
           </Card>
         </Col>
+        {/* Auction data/input */}
         <Col md={6}>
           <Card>
             <Card.Body>
+              {/* NFT info */}
               <Card.Title>RJ Barrett New York Knicks 2023-2024 Game Worn Statement Edition Jersey</Card.Title>
-              
+
               <Badge bg="success" className="mb-3">No reserve</Badge>
-              
+
+              {/* Show auction end date */}
               <div className="lot-details" style={{ margin: "5px" }}>
                 <span>Lot closes</span> <br />
                 <span>20:48:51 • January 25, 02:02 PM EST</span>
               </div>
 
               <div className='line' />
+              
+              {/* Show reserve price and highest accepted bid */}
               <div className="auction-estimate mb-2" style={{ margin: "5px" }}>
                 <span>Estimate</span><br />
                 <span>2,000 - 5,000 USD</span>
               </div>
               <div className='line' />
 
+              {/* Bid data */}
               <div className="current-bid mb-2" style={{ margin: "5px" }}>
                 <span>Current Bid</span> <br />
                 <span> 1,000 USD <Badge bg="secondary">12 Bids</Badge></span>
               </div>
+
               <div className='line' />
-              
+
+              {/* Show bid button or connect wallet button */}
               <ConnectWalletOrBid />
             </Card.Body>
           </Card>
@@ -55,8 +64,8 @@ function Auction() {
 
 function ConnectWalletOrBid() {
   const { isConnected } = useAccount()
-  
-  if(isConnected) {
+
+  if (isConnected) {
     return (
       <AuctionBidButton />
     )
