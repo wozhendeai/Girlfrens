b861171a61748efd48c4ee4bcf6579b80fe09fee
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-02-02 21:01:21 -0500

    use actual contracts in config

--- site/src/contractConfig.js
+++ site/src/contractConfig.js
@@ -1,5 +1,12 @@
 import abi from "../../contracts/artifacts/contracts/GirlfrenAuction.sol/GirlfrenAuction.json";
 
-const address = "";
+const config = {
+    treasuryStrategy: "0xf118C935b3E1EF1FA15E52bB101E87c912190691",
+    girlfrenTreasury: "0xdFC69949642670D3d781EDfED0868D6B8a9658f5",
+    girlfrenAuction: "0xD9224Cb60E18287724637DF217CD154ae0C6a95b",
+    girlfrenNFT: "0xf20cdD5A98acD5Ae1f0f771527394C93Dc9187Bf",
+    girlfrenTraitManager: "0xEf16754F214f2c8E957C295cBfD348D667fdd56c"
+}
 
-export { abi, address };
+
+export { abi, config };
