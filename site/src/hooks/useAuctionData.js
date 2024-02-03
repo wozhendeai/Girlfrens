414966628e86f46aa63053cb481b20a290918e07
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-02-03 09:29:52 -0500

    fix useAuctionData hook

--- site/src/hooks/useAuctionData.js
+++ site/src/hooks/useAuctionData.js
@@ -1,30 +1,32 @@
 import { useReadContract } from 'wagmi';
 import { abi, config } from '../contractConfig';
+import { blastSepolia } from 'viem/chains';
 
 const useAuctionData = () => {
   const { data, error, isLoading } = useReadContract({
-    addressOrName: config.girlfrenAuction, // GirlfrenAuction contract address
-    contractInterface: abi, // ABI of the GirlfrenAuction contract
-    functionName: 'auctionData',
+    address: config.girlfrenAuction,
+    abi: abi.abi,
+    chainId: blastSepolia.id,
   });
 
-  console.log(data, error, isLoading)
+  console.log(data)
 
   // Transform the raw data into a more friendly format
   const formattedData = data ? {
     bidder: data.bidder,
     amount: data.amount.toString(),
     withdrawable: data.withdrawable.toString(),
-    startTime: new Date(data.startTime.toNumber() * 1000).toISOString(),
-    endTime: new Date(data.endTime.toNumber() * 1000).toISOString(),
-    girlfrenId: data.girlfrenId.toNumber(),
+    // Incase the first auction hasn't started yet:
+    startTime: data.startTime !== 0 ? new Date(data.startTime * 1000).toISOString() : null,
+    endTime: data.endTime !== 0 ? new Date(data.endTime * 1000).toISOString() : null,
+    girlfrenId: data.girlfrenId,
     settled: data.settled,
     reservePercentage: data.reservePercentage,
     girlfrensNFT: data.girlfrensNFT,
     reservePrice: data.reservePrice.toString(),
     bidIncrement: data.bidIncrement.toString(),
-    duration: data.duration.toNumber(),
-    timeBuffer: data.timeBuffer.toNumber(),
+    duration: data.duration,
+    timeBuffer: data.timeBuffer,
     girlfrenBalance: data.girlfrenBalance.toString(),
     girlfrensTotalRedeemed: data.girlfrensTotalRedeemed.toString(),
   } : null;
