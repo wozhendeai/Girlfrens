db4717b808776d171897f131f2fabd5baebd72fc
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-02-03 08:43:50 -0500

    ask to switch chain if wrong one

--- site/src/components/ConnectWalletButton/ConnectWalletButton.jsx
+++ site/src/components/ConnectWalletButton/ConnectWalletButton.jsx
@@ -1,24 +1,42 @@
 import Button from 'react-bootstrap/Button';
-import { useAccount, useConnect } from 'wagmi';
+import { useAccount, useConnect, useSwitchChain } from 'wagmi';
+import { useEffect } from 'react';
+import { blastSepolia } from '@wagmi/core/chains';
 
 // eslint-disable-next-line react/prop-types
-function ConnectWalletButton({ variant, size, text, isButton = true }) { // Add isButton prop with default value true
+function ConnectWalletButton({ variant, size, text, isButton = true }) {
   const { connect, connectors } = useConnect();
-  const { isConnected } = useAccount();
+  const { isConnected, chain } = useAccount();
+  const { switchChain } = useSwitchChain();
   let connector = connectors[0];
+
+  // Handle the connection and potential chain switch
+  const handleConnectAndSwitch = async () => {
+    if (!isConnected) {
+      connect({ connector });
+    } else if (chain?.id !== blastSepolia.id && switchChain) {
+      switchChain(blastSepolia.id);
+    }
+  };
+
+  useEffect(() => {
+    if (isConnected && chain?.id !== blastSepolia.id) {
+      switchChain({chainId: blastSepolia.id});
+    }
+  }, [isConnected, chain, switchChain]);
 
   if (isButton) {
     return (
       <Button
         variant={variant}
         size={size}
-        onClick={() => connect({ connector })}
+        onClick={handleConnectAndSwitch}
       >
         {isConnected ? "CONNECTED" : text}
       </Button>
     );
   } else {
-    return <span onClick={() => connect({ connector })}>{isConnected ? "CONNECTED" : text}</span>;
+    return <span onClick={handleConnectAndSwitch}>{isConnected ? "CONNECTED" : text}</span>;
   }
 }
 
