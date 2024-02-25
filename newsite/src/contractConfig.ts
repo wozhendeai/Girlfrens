6fa5f32db42af40e5831716beb199482d3e6a20b
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-02-24 19:37:37 -0500

    auction page use backend

---
+++ newsite/src/contractConfig.ts
@@ -0,0 +1,12 @@
+import abi from "../../contracts/artifacts/contracts/GirlfrenAuction.sol/GirlfrenAuction.json";
+import GirlfrenNFTABI from "../../contracts/artifacts/contracts/GirlfrenNFT.sol/Girlfren.json";
+
+const config = {
+    treasuryStrategy: "0x75b1C7c88860c98B52B4C540aE7C2eFDFd5444F1" as `0x${string}`,
+    girlfrenTreasury: "0xB114665Bd2c39d9359480BcD861859EAb24809EF" as `0x${string}`,
+    girlfrenAuction: "0xa998c689E471AeE9E6f93b177CF4E90562903c62" as `0x${string}`,
+    girlfrenNFT: "0xBe30d4751F7661729e1d170B33D0fe2ED71C6Fa4" as `0x${string}`,
+}
+
+
+export { abi, GirlfrenNFTABI, config };
--- newsite/src/main.tsx
+++ newsite/src/main.tsx
@@ -7,6 +7,15 @@ import {
 import Root from './components/Root/Root.tsx'
 import { createTheme } from '@mui/material';
 import Shuriken from './fonts/Shuriken.ttf';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Wagmi
+import { WagmiProvider } from 'wagmi'
+import { http, createConfig } from 'wagmi'
+import { mainnet, sepolia, localhost } from 'wagmi/chains'
+import { blastSepolia } from '@wagmi/core/chains'
+
+import { injected } from 'wagmi/connectors'
 
 // Pages
 import Home from './pages/Home/Home.tsx';
@@ -33,6 +42,19 @@ const theme = createTheme({
   },
 });
 
+const config = createConfig({
+  chains: [blastSepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [blastSepolia.id]: http(),
+    [localhost.id]: http()
+  },
+  connectors: [
+    injected()
+  ]
+})
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -55,10 +77,16 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </WagmiProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
